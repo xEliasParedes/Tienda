ca33b9ae695314cee25366aed50198d51c831928
Vina del Mar <vina@duoc.acad>
2023-06-30 14:37:34 -0400

    modelos registrados en admin

--- producto/admin.py
+++ producto/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import comuna , region, envio, marca, provedor, producto
+
 # Register your models here.
+
+admin.site.register(producto)
+admin.site.register(provedor)
+admin.site.register(marca)
+admin.site.register(envio)
+admin.site.register(region)
+admin.site.register(comuna)
