914df6518ffb9486a0dea55d40b02a8bf4125c68
xEliasParedes <Paredeselias7@gmail.com>
2023-07-06 15:36:30 -0400

    restructuracion, incorporacion de funciones y mejora de estilos para principal

--- principal/urls.py
+++ principal/urls.py
@@ -1,10 +1,7 @@
-#from django.conf.urls import urls
+#from django.conf.urls import url
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
-    path('', views.index, name='index')
-
+    path ('',views.index, name='index'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def index(request):
     context={}
     return render(request, 'principal/index.html', context)
-
-def list_producto(request):
-    context={}
-    return render(request, 'producto/listado_producto.html', context)
