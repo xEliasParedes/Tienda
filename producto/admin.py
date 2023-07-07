bef44636c9067ea07f0e76c0b0a352d0af13fe17
xEliasParedes <Paredeselias7@gmail.com>
2023-07-06 21:38:23 -0400

    correcion cde data base

--- producto/admin.py
+++ producto/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from .models import producto, marca, provedor,despacho, retiro, sucursal, comuna, region,categoria, sub_categoria
+from .models import Producto, Marca, Provedor,Despacho, Retiro, Sucursal, Comuna, Region,Categoria, Sub_Categoria
 
 # Register your models here.
-admin.site.register(producto)
-admin.site.register(marca)
-admin.site.register(provedor)
-admin.site.register(despacho)
-admin.site.register(retiro)
-admin.site.register(sucursal)
-admin.site.register(comuna)
-admin.site.register(region)
-admin.site.register(categoria)
-admin.site.register(sub_categoria)
+admin.site.register(Producto)
+admin.site.register(Marca)
+admin.site.register(Provedor)
+admin.site.register(Despacho)
+admin.site.register(Retiro)
+admin.site.register(Sucursal)
+admin.site.register(Comuna)
+admin.site.register(Region)
+admin.site.register(Categoria)
+admin.site.register(Sub_Categoria)
 
 
 
--- producto/models.py
+++ producto/models.py
@@ -3,54 +3,56 @@ from dateutil.relativedelta import relativedelta
 
 # Create your models here.
 
-class producto(models.Model):
+
+
+class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=35)          
     cantidad = models.CharField(max_length=3)        
     descripcion = models.CharField(max_length=330)    
     precio_new = models.CharField(max_length=6)
     precio_old = models.CharField(max_length=6)     
-    marca =models.ForeignKey('marca',on_delete=models.CASCADE, db_column='id_marca')          
-    provedor =models.ForeignKey('provedor', on_delete=models.CASCADE, db_column='id_provedor')     
+    marca =models.ForeignKey('Marca',on_delete=models.CASCADE, db_column='id_marca')          
+    provedor =models.ForeignKey('Provedor', on_delete=models.CASCADE, db_column='id_provedor')     
     despacho = models.BooleanField()    
     retiro =  models.BooleanField() 
-    sub_categoria = models.ForeignKey('sub_categoria', on_delete=models.CASCADE, db_column='id_sub_categoria')
+    sub_categoria = models.ForeignKey('Sub_Categoria', on_delete=models.CASCADE, db_column='id_sub_categoria')
 
     def __str__(self):
         return str(self.nombre)
 
 
-class marca(models.Model):
+class Marca(models.Model):
     id_marca = models.CharField(primary_key=True, max_length= 2, db_column='id_marca')  
     nombre = models.CharField(max_length=35)
-    categoria = models.ForeignKey('categoria', on_delete=models.CASCADE, db_column='id_categoria') 
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, db_column='id_categoria') 
 
     def __str__(self):
         return str(self.nombre) 
 
-class provedor(models.Model):
+class Provedor(models.Model):
     id_provedor = models.CharField(primary_key=True, max_length=2, db_column='id_provedor')
     nombre = models.CharField(max_length=35)
 
     def __str__(self):
-        return "provedor: "+str(self.nombre)
+        return str(self.nombre)
 
 
-class despacho(models.Model):
+class Despacho(models.Model):
     id_soli_des = models.AutoField(primary_key=True, db_column= 'id_soli_des')
     direccion = models.CharField(max_length=100)  
     num_casa_dep= models.CharField(max_length=3, blank=True, null=True)
     info_add = models.CharField(max_length=100, blank=True, null=True)  
-    comuna = models.ForeignKey('comuna', on_delete=models.CASCADE,db_column='id_comuna')
+    comuna = models.ForeignKey('Comuna', on_delete=models.CASCADE,db_column='id_comuna')
     receptor =  models.CharField(max_length=65)
 
     def __str__(self):
         return str(self.id_soli_des)+"-"+str(self.direccion)+"-"+str(self.comuna)
 
-class retiro(models.Model):
+class Retiro(models.Model):
     id_soli_ret = models.AutoField(primary_key=True,  db_column= 'id_soli_ret')
     fecha_estimada = relativedelta(days=15)
-    sucursal= models.ForeignKey('sucursal', on_delete=models.CASCADE, db_column='id_sucursal')
+    sucursal= models.ForeignKey('Sucursal', on_delete=models.CASCADE, db_column='id_sucursal')
     receptor =  models.CharField(max_length=65)
     
     def __str__(self):
@@ -58,31 +60,31 @@ class retiro(models.Model):
 
 
 
-class sucursal(models.Model):
+class Sucursal(models.Model):
     id_sucursal = models.CharField(primary_key=True, max_length=4)
     direccion = models.CharField(max_length=100) 
     nombre = models.CharField(max_length=50)
-    comuna = models.ForeignKey('comuna', on_delete=models.CASCADE, db_column='id_comuna')
+    comuna = models.ForeignKey('Comuna', on_delete=models.CASCADE, db_column='id_comuna')
 
     def __str__(self):
         return str(self.direccion)
 
-class comuna(models.Model):
+class Comuna(models.Model):
     id_comuna = models.AutoField(primary_key=True,  db_column='id_comuna')
     nombre = models.CharField(max_length=20)
-    region = models.ForeignKey('region', on_delete=models.CASCADE, db_column='id_region')
+    region = models.ForeignKey('Region', on_delete=models.CASCADE, db_column='id_region')
 
     def __str__(self):
         return str(self.nombre)+"-"+str(self.region)
 
-class region(models.Model):
+class Region(models.Model):
     id_region = models.AutoField(primary_key=True,  db_column='id_region')
     nombre = models.CharField(max_length=20)
 
     def __str__(self):
         return str(self.nombre)
 
-class categoria(models.Model):
+class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True, db_column= 'id_categoria')
     nombre = models.CharField(max_length=20)
 
@@ -90,10 +92,10 @@ class categoria(models.Model):
         return str(self.nombre)
 
 
-class sub_categoria(models.Model):
+class Sub_Categoria(models.Model):
     id_sub_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=20)
-    categoria = models.ForeignKey('categoria', on_delete=models.CASCADE, db_column='id_categoria')
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, db_column='id_categoria')
 
 
     def __str__(self):
